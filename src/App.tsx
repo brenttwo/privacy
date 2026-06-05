54251711a58c032dcff42174c978164f3b440b86
signaajames <penguingnop@gmail.com>
2026-06-05 12:52:03 +0800

    fixed og

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
         Example:
       </p>
 
-      <pre>IP:      123.123.123.123<br/>Hash:    8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92</pre>
+      <pre>IP:    123.123.123.123<br/>Hash:  8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92</pre>
 
       <p>
         That hash is all that ever gets stored. The original IP never reaches
